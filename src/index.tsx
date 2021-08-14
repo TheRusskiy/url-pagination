f6880e89f335603a131be586ba9762d5350caf7c
Dmitry Ishkov <dimanrussian@gmail.com>
2021-08-14 17:18:52 +0400

    another exports change

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
-export * as useUrlPagination from './useUrlPagination';
-export * as usePagination from './usePagination';
-export * as calculatePage from './utils/calculatePage';
-export * as calculateOffset from './utils/calculateOffset';
+import _useUrlPagination from './useUrlPagination';
+import _usePagination from './usePagination';
+import _calculatePage from './utils/calculatePage';
+import _calculateOffset from './utils/calculateOffset';
+
+export const useUrlPagination = _useUrlPagination;
+export const usePagination = _usePagination;
+export const calculatePage = _calculatePage;
+export const calculateOffset = _calculateOffset;
