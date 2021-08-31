48ea524fdd9b4a655636f65b16a0624674d7bab7
Dmitry Ishkov <dimanrussian@gmail.com>
2021-08-31 19:00:19 +0400

    expose pushWithParams and addParams

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,5 @@ export { default as useUrlPagination } from './useUrlPagination';
 export { default as usePagination } from './usePagination';
 export { default as calculatePage } from './utils/calculatePage';
 export { default as calculateOffset } from './utils/calculateOffset';
+export { default as pushWithParams } from './utils/pushWithParams';
+export { default as addParams } from './utils/addParams';
